3014c09c849d3a11ccf6be7125655c6f46bf9288
Nitish Goyal <gnitish@gnitish-a01.vmware.com>
2019-05-15 04:01:21 +0530

    changes

--- with/gen_rsa_keys.py
+++ with/gen_rsa_keys.py
@@ -6,5 +6,8 @@ file_out = open("keys/private.pem", "wb")
 file_out.write(private_key)
 
 public_key = key.publickey().export_key()
-file_out = open("keys/public.pem", "wb")
+file_out = open("keys/public.js", "wb")
+content = "var pem_file = `"
+file_out.write(content.encode('utf-8'))
 file_out.write(public_key)
+file_out.write("`".encode('utf-8'))
